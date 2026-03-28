175a083e78fc4d7b90f5752cc66fa199c70b67fe
Peng Chen <roccen@gmail.com>
2026-03-28 17:46:19 +0800

    Initial Commit

---
+++ VoiceKey/VoiceKeyApp.swift
@@ -0,0 +1,17 @@
+//
+//  VoiceKeyApp.swift
+//  VoiceKey
+//
+//  Created by Peng Chen on 28/3/26.
+//
+
+import SwiftUI
+
+@main
+struct VoiceKeyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
